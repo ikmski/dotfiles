96ad1567711385f7852ffa3a236f3ff0c302009f
masaki <ikeda.masaki@gmail.com>
2014-03-27 00:20:40 +0900

    add git_template

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.git_template ~/.git_template
