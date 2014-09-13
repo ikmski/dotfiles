acdf4eb2e162aa2bfde82907180a43303f6b5ba5
masaki <ikeda.masaki@gmail.com>
2014-09-13 13:44:47 +0900

    bash_profile追加

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+$ cat ~/.bash_profile
+if [ -f ~/.bashrc ] ; then
+. ~/.bashrc
+fi
+
+#if which rbenv > /dev/null; then
+    eval "$(rbenv init -)";
+#fi
+
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/zsh
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
