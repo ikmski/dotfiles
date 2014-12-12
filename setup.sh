95a860a3cda9f2706f400562e53abff7e69d71ce
Masaki Ikeda <ikeda.masaki@tecotec.co.jp>
2014-12-12 06:17:30 +0000

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #! /bin/zsh
 
-ln -s -b ~/dotfiles/.vimrc ~/.vimrc
-ln -s -b ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s -b ~/dotfiles/.bashrc ~/.bashrc
-ln -s -b ~/dotfiles/.zshrc ~/.zshrc
-ln -s -b ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s -b ~/dotfiles/.git_template/ ~/.git_template
-ln -s -b ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.git_template/ ~/.git_template
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-mkdir ~/.vim/bundle
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/tmp
