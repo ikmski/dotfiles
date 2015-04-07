dba4093aee14d8f709658bd13a989a40dd56c994
masaki <ikeda.masaki@gmail.com>
2015-04-07 17:21:19 +0900

    [modify] setup.sh 修正

--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
-#! /bin/zsh
+#!/bin/bash
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.git_template/ ~/.git_template
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+DOT_FILES=(
+ .vimrc
+ .bash_profile
+ .bashrc
+ .zshrc
+ .gitconfig
+ .git_template/
+ .tmux.conf
+)
+
+for file in ${DOT_FILES[@]}
+do
+  if [ -a ${HOME}/${file} ]; then
+    if [ -L $HOME/$file ]; then
+      rm -f ${HOME}/${file}
+      ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
+    elif [ -d $HOME/$file ]; then
+      :
+    else
+      mv ${HOME}/${file} ${HOME}/${file}.bak
+      ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
+    fi
+  fi
+done
 
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/tmp
