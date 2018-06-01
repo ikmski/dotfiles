4707509f09290541d20ca44029d3d385c1bbdf47
masaki <ikeda.masaki@gmail.com>
2018-06-01 09:57:45 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-mkdir -p ~/.config
-mkdir -p ~/.cache
-mkdir -p ~/.vim/tmp
+mkdir -p ~/.config/
+mkdir -p ~/.cache/
+mkdir -p ~/.vim/tmp/
 
 DOT_FILES=(
+ .config/peco
+ .git_template
+ .bashrc
+ .bash_profile
+ .gitconfig
+ .my.cnf
+ .tmux.conf
  .vimrc
  .zshrc
- .gitconfig
- .git_template/
- .config/vim/
- .config/peco/
- .tmux.conf
- .my.cnf
 )
 
 for file in ${DOT_FILES[@]}
 do
   if [ -a ${HOME}/${file} ]; then
-    if [ -L $HOME/$file ]; then
+    if [ -L $HOME/${file} ]; then
       rm -f ${HOME}/${file}
-      ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
-    elif [ -d $HOME/$file ]; then
-      :
     else
       mv ${HOME}/${file} ${HOME}/${file}.bak
-      ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
     fi
-  else
-    ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
   fi
+  ln -s ${HOME}/dotfiles/${file} ${HOME}/${file}
 done
 
